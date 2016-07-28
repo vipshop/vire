4f1dd6658aeef67f5f0eff4adbbe05f20ec4de14
deep011 <diguo58@gmail.com>
2016-07-28 12:45:50 +0800

    add 'DECR simple test'

--- tests/vrt_simple.c
+++ tests/vrt_simple.c
@@ -252,6 +252,71 @@ error:
     return 0;
 }
 
+static int simple_test_cmd_decr(vire_instance *vi)
+{
+    char *key = "test_cmd_decr-key";
+    long long n = 0, incr_times = 100;
+    char *MESSAGE = "DECR simple test";
+    redisReply * reply = NULL;
+
+    reply = redisCommand(vi->ctx, "del %s", key);
+    if (reply == NULL || reply->type == REDIS_REPLY_ERROR) {
+        goto error;
+    }
+    freeReplyObject(reply);
+
+    while (n < incr_times) {
+        reply = redisCommand(vi->ctx, "decr %s", key);
+        if (reply == NULL || reply->type != REDIS_REPLY_INTEGER || 
+            reply->integer + n != -1) {
+            vrt_scnprintf(errmsg, LOG_MAX_LEN, "incr %lld times error", n+1);
+            goto error;
+        }
+        freeReplyObject(reply);
+        
+        n ++;
+    }
+
+    reply = redisCommand(vi->ctx, "get %s", key);
+    if (reply == NULL || reply->type != REDIS_REPLY_STRING) {
+        goto error;
+    } else {
+        long long value;
+        if (!string2ll(reply->str,reply->len,&value) || value + incr_times != 0) {
+            vrt_scnprintf(errmsg, LOG_MAX_LEN, "decr to -%lld error, %s in fact", 
+                incr_times, reply->str);
+            goto error;
+        }
+    }
+    freeReplyObject(reply);
+
+    reply = redisCommand(vi->ctx, "set %s %s", key, "a");
+    if (reply == NULL || reply->type != REDIS_REPLY_STATUS || 
+        reply->len != 2 || strcmp(reply->str,"OK")) {
+        goto error;
+    }
+    freeReplyObject(reply);
+
+    reply = redisCommand(vi->ctx, "incr %s", key);
+    if (reply == NULL || reply->type != REDIS_REPLY_ERROR) {
+        goto error;
+    }
+    freeReplyObject(reply);
+
+    show_test_result(VRT_TEST_OK,MESSAGE,errmsg);
+
+    return 1;
+
+error:
+
+    if (reply) freeReplyObject(reply);
+
+    show_test_result(VRT_TEST_ERR,MESSAGE,errmsg);
+    errmsg[0] = '\0';
+
+    return 0;
+}
+
 void simple_test(void)
 {
     vire_instance *vi;
@@ -270,6 +335,7 @@ void simple_test(void)
     ok_count+=simple_test_cmd_setex(vi);
     ok_count+=simple_test_cmd_psetex(vi);
     ok_count+=simple_test_cmd_incr(vi);
+    ok_count+=simple_test_cmd_decr(vi);
     
     vire_instance_destroy(vi);
 }
