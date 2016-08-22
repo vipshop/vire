7cbee24485ec031186aadc77a3c5fcb3d3976b23
deep011 <diguo58@gmail.com>
2016-08-22 12:45:35 +0800

    add test command 'zscore' for vireabtest

--- tests/vrt_produce_data.c
+++ tests/vrt_produce_data.c
@@ -717,6 +717,21 @@ static data_unit *zrevrank_cmd_producer(data_producer *dp, produce_scheme *ps)
     return du;
 }
 
+static data_unit *zscore_cmd_producer(data_producer *dp, produce_scheme *ps)
+{
+    data_unit *du;
+    
+    du = data_unit_get();
+    du->dp = dp;
+    du->argc = 3;
+    du->argv = malloc(du->argc*sizeof(sds));
+    du->argv[0] = sdsnew(dp->name);
+    du->argv[1] = get_random_key_with_hit_ratio(ps);
+    du->argv[2] = get_random_string();
+        
+    return du;
+}
+
 static int producers_count;
 data_producer redis_data_producer_table[] = {
     /* Key */
@@ -752,7 +767,8 @@ data_producer redis_data_producer_table[] = {
     {"zrangebyscore",zrangebyscore_cmd_producer,-4,"r",0,NULL,1,1,1,TEST_CMD_TYPE_ZSET},
     {"zrevrangebyscore",zrevrangebyscore_cmd_producer,-4,"r",0,NULL,1,1,1,TEST_CMD_TYPE_ZSET},
     {"zrank",zrank_cmd_producer,3,"rF",0,NULL,1,1,1,TEST_CMD_TYPE_ZSET},
-    {"zrevrank",zrevrank_cmd_producer,3,"rF",0,NULL,1,1,1,TEST_CMD_TYPE_ZSET}
+    {"zrevrank",zrevrank_cmd_producer,3,"rF",0,NULL,1,1,1,TEST_CMD_TYPE_ZSET},
+    {"zscore",zscore_cmd_producer,3,"rF",0,NULL,1,1,1,TEST_CMD_TYPE_ZSET}
 };
 
 data_unit *data_unit_get(void)
