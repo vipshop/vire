05422fffbac575a2e805e8717002c3ff9fb20252
deep011 <diguo58@gmail.com>
2016-09-05 17:17:17 +0800

    vireabtest: add test command 'mget'

--- tests/vrt_produce_data.c
+++ tests/vrt_produce_data.c
@@ -784,6 +784,20 @@ static data_unit *bitpos_cmd_producer(data_producer *dp, produce_scheme *ps)
     return du;
 }
 
+static data_unit *mget_cmd_producer(data_producer *dp, produce_scheme *ps)
+{
+    data_unit *du;
+
+    du = data_unit_get();
+    du->dp = dp;
+    du->argc = 2;
+    du->argv = malloc(du->argc*sizeof(sds));
+    du->argv[0] = sdsnew(dp->name);
+    du->argv[1] = get_random_key_with_hit_ratio(ps,dp);
+    
+    return du;
+}
+
 static data_unit *rpush_cmd_producer(data_producer *dp, produce_scheme *ps)
 {
     data_unit *du;
@@ -1349,6 +1363,7 @@ data_producer redis_data_producer_table[] = {
     {"getrange",getrange_cmd_producer,4,"r",0,NULL,1,1,1,TEST_CMD_TYPE_STRING,NULL},
     {"bitcount",bitcount_cmd_producer,-2,"r",0,NULL,1,1,1,TEST_CMD_TYPE_STRING,NULL},
     {"bitpos",bitpos_cmd_producer,-3,"r",0,NULL,1,1,1,TEST_CMD_TYPE_STRING,0},
+    {"mget",mget_cmd_producer,-2,"r",0,NULL,1,-1,1,TEST_CMD_TYPE_STRING,0},
     /* List */
     {"rpush",rpush_cmd_producer,-3,"wmFA",0,NULL,1,1,1,TEST_CMD_TYPE_LIST,rpush_cmd_nck},
     {"lpush",lpush_cmd_producer,-3,"wmFA",0,NULL,1,1,1,TEST_CMD_TYPE_LIST,lpush_cmd_nck},
