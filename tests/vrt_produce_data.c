4547c00a064091777edb851473dddba6ef10cd38
deep011 <diguo58@gmail.com>
2016-09-12 17:50:38 +0800

    vireabtest: add test command 'hlen'

--- tests/vrt_produce_data.c
+++ tests/vrt_produce_data.c
@@ -841,6 +841,20 @@ static data_unit *hget_cmd_producer(data_producer *dp, produce_scheme *ps)
     return du;
 }
 
+static data_unit *hlen_cmd_producer(data_producer *dp, produce_scheme *ps)
+{
+    data_unit *du;
+
+    du = data_unit_get();
+    du->dp = dp;
+    du->argc = 2;
+    du->argv = malloc(du->argc*sizeof(sds));
+    du->argv[0] = sdsnew(dp->name);
+    du->argv[1] = get_random_key_with_hit_ratio(ps,dp);
+    
+    return du;
+}
+
 static data_unit *hmset_cmd_producer(data_producer *dp, produce_scheme *ps)
 {
     data_unit *du;
@@ -1556,6 +1570,7 @@ data_producer redis_data_producer_table[] = {
     /* Hash */
     {"hset",hset_cmd_producer,4,"wmFA",0,NULL,1,1,1,TEST_CMD_TYPE_HASH,nck_when_one},
     {"hget",hget_cmd_producer,3,"rF",0,NULL,1,1,1,TEST_CMD_TYPE_HASH,NULL},
+    {"hlen",hlen_cmd_producer,2,"rF",0,NULL,1,1,1,TEST_CMD_TYPE_HASH,NULL},
     {"hmset",hmset_cmd_producer,-4,"wmA",0,NULL,1,1,1,TEST_CMD_TYPE_HASH,nck_when_ok},
     /* List */
     {"rpush",rpush_cmd_producer,-3,"wmFA",0,NULL,1,1,1,TEST_CMD_TYPE_LIST,rpush_cmd_nck},
